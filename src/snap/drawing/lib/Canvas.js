e636f3ed79bf8d26611f11a2b1d952cc173bc297
bheilman <das.ist.junk@gmail.com>
2013-11-07 20:09:50 -0500

    Removing glyphing and drawing, as they should be separate projects

--- src/snap/drawing/lib/Canvas.js
+++
@@ -1,8 +0,0 @@
-;(function( global, undefined ){
-	bMoor.constructor.define({
-		name : 'Canvas',
-		namespace : ['snap','drawing','lib'],
-		construct : function( obj ){},
-		properties : {}
-	});
-}( this ));
